dddd3c52188038d7c8a020c017ef413125661487
Pablo C. Ruíz <pcruiher089@gmail.com>
2023-12-01 12:35:13 -0600

    series

--- pi.py
+++ pi.py
@@ -1,5 +1,3 @@
-#test
-
 import time
 import board
 import neopixel
@@ -9,31 +7,21 @@ pixel_pin = board.D18
 
 pixels = neopixel.NeoPixel(pixel_pin, num_pixels, brightness=0.2, auto_write=False, pixel_order=neopixel.RGB)
 
-def rainbow_cycle(wait):
-    for j in range(255):
-        for i in range(num_pixels):
-            pixel_index = (i * 256 // num_pixels) + j
-            pixels[i] = wheel(pixel_index & 255)
+def turn_on_one_by_one():
+    for i in range(num_pixels):
+        pixels[i] = (255, 255, 255) 
         pixels.show()
-        time.sleep(wait)
+        time.sleep(0.1)  
 
-def wheel(pos):
-    if pos < 85:
-        return (pos * 3, 255 - pos * 3, 0)
-    elif pos < 170:
-        pos -= 85
-        return (255 - pos * 3, 0, pos * 3)
-    else:
-        pos -= 170
-        return (0, pos * 3, 255 - pos * 3)
-
-try:
-    while True:
-        rainbow_cycle(0.01)
-
-except KeyboardInterrupt:
+def turn_off_all():
     pixels.fill((0, 0, 0))
     pixels.show()
 
+try:
+    while True:
+        turn_on_one_by_one()
+        time.sleep(1)  
+        turn_off_all()
 
-
+except KeyboardInterrupt:
+    turn_off_all()
