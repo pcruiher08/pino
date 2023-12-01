20adf738474b995fd255837178d94df82ea7b797
Pablo C. Ruíz <pcruiher089@gmail.com>
2023-12-01 13:11:29 -0600

    color correction

--- pi.py
+++ pi.py
@@ -2,7 +2,7 @@ import time
 import board
 import neopixel
 
-num_pixels = 200
+num_pixels = 100
 pixel_pin = board.D18
 
 pixels = neopixel.NeoPixel(pixel_pin, num_pixels, brightness=0.2, auto_write=False, pixel_order=neopixel.RGB)
@@ -19,28 +19,30 @@ def turn_on_one_by_one():
             apply_gamma_correction(255),
             apply_gamma_correction(255),
         )
-
-        for brightness_value in range(1, 10):
-            pixels.brightness = brightness_value / 10.0
-            pixels.show()
-            time.sleep(0.5)
-
-        turn_off_all()
-
-        for brightness_value in range(9, 0, -1):
-            pixels.brightness = brightness_value / 10.0
-            pixels.show()
-            time.sleep(0.5)
-
         pixels.show()
-        time.sleep(0.01)
+        time.sleep(0.1)  
 
 def turn_off_all():
+    for brightness_value in range(1, 10):
+        pixels.brightness = brightness_value / 10.0
+        pixels.show()
+        time.sleep(0.5) 
+
+    turn_off_all()
+
+    for brightness_value in range(9, 0, -1):
+        pixels.brightness = brightness_value / 10.0
+        pixels.show()
+        time.sleep(0.5)  
+
     pixels.fill((0, 0, 0))
     pixels.show()
 
-
 try:
-    turn_on_one_by_one()
+    while True:
+        turn_on_one_by_one()
+        time.sleep(1)  
+        turn_off_all()
+
 except KeyboardInterrupt:
     turn_off_all()
