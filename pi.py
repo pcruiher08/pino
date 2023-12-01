810365cd26a313956dc24015a73af4a23e214747
Pablo C. Ruíz <pcruiher089@gmail.com>
2023-12-01 12:16:44 -0600

    rainbow

--- pi.py
+++ pi.py
@@ -1,2 +1,39 @@
 #test
-print("working")
+
+import time
+import board
+import neopixel
+
+num_pixels = 50
+pixel_pin = board.D18
+
+pixels = neopixel.NeoPixel(pixel_pin, num_pixels, brightness=0.2, auto_write=False, pixel_order=neopixel.RGB)
+
+def rainbow_cycle(wait):
+    for j in range(255):
+        for i in range(num_pixels):
+            pixel_index = (i * 256 // num_pixels) + j
+            pixels[i] = wheel(pixel_index & 255)
+        pixels.show()
+        time.sleep(wait)
+
+def wheel(pos):
+    if pos < 85:
+        return (pos * 3, 255 - pos * 3, 0)
+    elif pos < 170:
+        pos -= 85
+        return (255 - pos * 3, 0, pos * 3)
+    else:
+        pos -= 170
+        return (0, pos * 3, 255 - pos * 3)
+
+try:
+    while True:
+        rainbow_cycle(0.01)
+
+except KeyboardInterrupt:
+    pixels.fill((0, 0, 0))
+    pixels.show()
+
+
+
