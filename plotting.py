7fcf3fff642a56cad6f66c41023ec3dd69498951
Pablo C. Ruíz <pcruiher089@gmail.com>
2023-12-03 19:30:54 -0600

    fixing another orientation issue

--- plotting.py
+++ plotting.py
@@ -16,6 +16,7 @@ ordered_leds = sorted(led_points, key=lambda point: point["x_corrected"])
 
 for point in ordered_leds:
     point["y_corrected"] = -point["y_corrected"]
+    point["x_corrected"] = -point["x_corrected"]
 
 pixels = neopixel.NeoPixel(PIXEL_PIN, NUM_PIXELS, brightness=0.5, auto_write=False)
 
--- testpoints.py
+++ testpoints.py
@@ -4,9 +4,13 @@ import matplotlib.pyplot as plt
 input_filename = "led_coordinates.json"
 with open(input_filename, "r") as json_file:
     led_points = json.load(json_file)
-    
+
 for point in led_points:
     point["y_corrected"] = -point["y_corrected"]
+    point["x_corrected"] = -point["x_corrected"]
+
+
+
 
 x_coordinates = [point["x_corrected"] for point in led_points]
 y_coordinates = [point["y_corrected"] for point in led_points]
