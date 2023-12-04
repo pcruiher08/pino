ec11cb7cf71104be1fb59797552d60a2e8ff1a0b
Pablo C. Ruíz <pcruiher089@gmail.com>
2023-12-03 19:29:39 -0600

    fixing orientation

--- plotting.py
+++ plotting.py
@@ -7,10 +7,15 @@ input_filename = "led_coordinates.json"
 with open(input_filename, "r") as json_file:
     led_points = json.load(json_file)
 
+
+
 NUM_PIXELS = len(led_points)  
 PIXEL_PIN = board.D18  
 
 ordered_leds = sorted(led_points, key=lambda point: point["x_corrected"])
+
+for point in ordered_leds:
+    point["y_corrected"] = -point["y_corrected"]
 
 pixels = neopixel.NeoPixel(PIXEL_PIN, NUM_PIXELS, brightness=0.5, auto_write=False)
 
---
+++ testpoints.py
@@ -0,0 +1,22 @@
+import json
+import matplotlib.pyplot as plt
+
+input_filename = "led_coordinates.json"
+with open(input_filename, "r") as json_file:
+    led_points = json.load(json_file)
+    
+for point in led_points:
+    point["y_corrected"] = -point["y_corrected"]
+
+x_coordinates = [point["x_corrected"] for point in led_points]
+y_coordinates = [point["y_corrected"] for point in led_points]
+
+plt.scatter(x_coordinates, y_coordinates, marker='o', label='LED Points')
+
+plt.xlabel('X Coordinate')
+plt.ylabel('Y Coordinate')
+plt.title('LED Points in 2D Plane')
+
+plt.legend()
+
+plt.show()
