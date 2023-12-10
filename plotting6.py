bc9a56d8af0df76adef910987185beac284dc040
Pablo C. Ruíz <pcruiher089@gmail.com>
2023-12-10 17:48:30 -0600

    fire

--- plotting6.py
+++ plotting6.py
@@ -9,7 +9,7 @@ pixels = neopixel.NeoPixel(board.D18, num_pixels, auto_write=False)
 
 def update_pixels():
     for i in range(num_pixels):
-        brightness = random.uniform(0.7, 1.0)  
+        brightness = random.uniform(0.4, 1.0)  
 
         red = int(100 * brightness)
         green = int(255 * i / (num_pixels / 2)) if i < num_pixels / 2 else int(255 * (1 - (i - num_pixels / 2) / (num_pixels / 2)))
