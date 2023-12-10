5a5f26e11032b6b22e6153978d10a05da26a19d7
Pablo C. Ruíz <pcruiher089@gmail.com>
2023-12-10 17:45:05 -0600

    fire

--- plotting5.py
+++ plotting5.py
@@ -8,10 +8,10 @@ pixels = neopixel.NeoPixel(board.D18, num_pixels, auto_write=False)
 
 def update_pixels():
     for i in range(num_pixels):
-        brightness = random.uniform(0.7, 1.0) 
+        brightness = random.uniform(0.4, 1.0) 
         color = [
             int(0 * brightness),  
-            int(165 * brightness),  
+            int(0 * brightness),  
             int(255 * brightness)     
         ]
         pixels[i] = tuple(color)
