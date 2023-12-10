fb3c7275aa796c60c36678b47cdb6c2dee7aa115
Pablo C. Ruíz <pcruiher089@gmail.com>
2023-12-10 17:34:47 -0600

    more points plotting math

--- plotting3.py
+++ plotting3.py
@@ -2,6 +2,7 @@ import time
 import board
 import neopixel
 import numpy as np
+import colorsys
 
 num_pixels = 500
 
@@ -9,19 +10,18 @@ pixels = neopixel.NeoPixel(board.D18, num_pixels, auto_write=False)
 
 def update_pixels(frame):
     for i in range(num_pixels):
+
         frequency = 0.1
         amplitude = 0.5
         phase = frame / 10.0
-        color = [
-            max(0, min(255, int(255 * amplitude * np.sin(2 * np.pi * frequency * i + phase)))),
-            max(0, min(255, int(255 * amplitude * np.sin(2 * np.pi * frequency * i + phase)))),
-            max(0, min(255, int(255 * amplitude * np.sin(2 * np.pi * frequency * i + phase))))
-        ]
-
+        hue = (np.sin(2 * np.pi * frequency * i + phase) + 1) / 2.0  
+        rgb_color = colorsys.hsv_to_rgb(hue, 1.0, 1.0)  
+        color = [int(255 * channel) for channel in rgb_color]
         pixels[i] = tuple(color)
+
     pixels.show()
 
 
-num_frames = 100
+num_frames = 500
 for frame in range(num_frames):
     update_pixels(frame)
