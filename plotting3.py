e49cbaeab2d0fa2273833ce3dadf99c4ee03d779
Pablo C. Ruíz <pcruiher089@gmail.com>
2023-12-10 17:33:01 -0600

    more points plotting math

--- plotting3.py
+++ plotting3.py
@@ -10,14 +10,13 @@ pixels = neopixel.NeoPixel(board.D18, num_pixels, auto_write=False)
 
 def update_pixels(frame):
     for i in range(num_pixels):
-        frequency = 0.5
-        amplitude = 0.7
-        phase = frame / 5.0
+        frequency = 2.0
+        amplitude = 0.3
+        phase = frame / 2.0
         hue = (np.sin(2 * np.pi * frequency * i + phase) + 1) / 2.0
         rgb_color = colorsys.hsv_to_rgb(hue, 1.0, 1.0)
         color = [int(255 * amplitude * channel) for channel in rgb_color]
         pixels[i] = tuple(color)
-
     pixels.show()
 
 num_frames = 5000
