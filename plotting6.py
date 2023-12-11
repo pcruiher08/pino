0498764f188b400cfe0a1f45ba224cd13e40b31e
Pablo C. Ruíz <pcruiher089@gmail.com>
2023-12-10 18:10:36 -0600

    ad

--- plotting6.py
+++ plotting6.py
@@ -9,13 +9,11 @@ pixels = neopixel.NeoPixel(board.D18, num_pixels, auto_write=False)
 
 def update_pixels():
     for i in range(num_pixels):
-        gradient = i / (num_pixels - 1)
+        gradient = i / (num_pixels - 1) + 200
         red = int(255 * (1 - gradient))
         green = int(255 * gradient)
         blue = 0
         pixels[num_pixels - i - 1] = (red, green, blue)
-        print(red, green, blue)
-
 
     pixels.show()
     time.sleep(0.02)
