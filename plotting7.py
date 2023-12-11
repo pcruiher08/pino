2da0f6515f172400eafab723e5e292718f7a5165
Pablo C. Ruíz <pcruiher089@gmail.com>
2023-12-10 18:23:52 -0600

    adddd

--- plotting7.py
+++ plotting7.py
@@ -2,18 +2,19 @@ import time
 import board
 import neopixel
 import random
-import math as m
+import math
 num_pixels = 500
 
 pixels = neopixel.NeoPixel(board.D18, num_pixels, auto_write=False)
 
 def update_pixels():
-    for t in range(1000):
+    for t in range(1000):  
         for i in range(num_pixels):
-            brightness = int(127.5 + 127.5 * m.cos(2 * m.pi * i / num_pixels + t / 50))
-            pixels[i] = (0, brightness, 0)
+            red = int(127.5 + 127.5 * math.cos(2 * math.pi * i / num_pixels + t / 50))
+            green = int(127.5 + 127.5 * math.cos(2 * math.pi * i / num_pixels + t / 30))
+            blue = int(127.5 + 127.5 * math.cos(2 * math.pi * i / num_pixels + t / 20))
+            pixels[i] = (red, green, blue)
         pixels.show()
-        time.sleep(0.02)  
 
 while True:
     update_pixels()
