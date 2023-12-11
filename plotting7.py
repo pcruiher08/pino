b35d29a92b3dee8fc556a68e6707c1e34666fc8c
Pablo C. Ruíz <pcruiher089@gmail.com>
2023-12-10 18:26:59 -0600

    add

--- plotting7.py
+++ plotting7.py
@@ -1,20 +1,28 @@
 import time
 import board
 import neopixel
-import random
 import math
+
 num_pixels = 500
 
 pixels = neopixel.NeoPixel(board.D18, num_pixels, auto_write=False)
 
-def update_pixels():
-    for t in range(1000):  
+colors = [
+    (255, 0, 0),   #Red
+    (0, 255, 0),   #Green
+    (0, 0, 255),   #Blue
+    (255, 255, 0), #Yellow
+    (255, 0, 255), #Magenta
+    (0, 255, 255), #Cyan
+]
+
+def color_variation_animation():
+    for t in range(1000):
         for i in range(num_pixels):
-            red = int(127.5 + 127.5 * math.cos(2 * math.pi * i / num_pixels + t / 50))
-            green = int(127.5 + 127.5 * math.cos(2 * math.pi * i / num_pixels + t / 30))
-            blue = int(127.5 + 127.5 * math.cos(2 * math.pi * i / num_pixels + t / 20))
-            pixels[i] = (red, green, blue)
-    pixels.show()
+            color_index = int((i / num_pixels) * len(colors))
+            pixels[i] = colors[color_index]
+        pixels.show()
+    
 
 while True:
-    update_pixels()
+    color_variation_animation()
